2a2a47674c47a2197fe5103842b1ee888f548a3d
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-08-25 10:09:12 -0700

    full screen

--- ZoomTransitionsWith18iOS/DetailView2.swift
+++ ZoomTransitionsWith18iOS/DetailView2.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct DetailView2: View {
     let image: MyImage
     let transitionName: Namespace.ID
+    @State private var showFull = false
     var body: some View {
         VStack {
             Image(image.imageName)
@@ -18,6 +19,16 @@ struct DetailView2: View {
                 .frame(maxWidth: .infinity)
                 .clipShape(RoundedRectangle(cornerRadius: 20))
                 .shadow(radius: 10)
+                .matchedTransitionSource(id: image, in: transitionName)
+                .onTapGesture {
+                    showFull.toggle()
+                }
+                .fullScreenCover(isPresented: $showFull) {
+                    NavigationStack {
+                        DetailView(image: image)
+                            .navigationTransition(.zoom(sourceID: image, in: transitionName))
+                    }
+                }
             Text(image.name)
                 .font(.title)
             Text(image.info)
