a5d4ad0ed14fb04f130f5f3e15b7a658183e6519
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-08-25 09:05:34 -0700

    Initial Commit

---
+++ ZoomTransitionsWith18iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZoomTransitionsWith18iOS
+//
+//  Created by test on 8/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZoomTransitionsWith18iOS/ZoomTransitionsWith18iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoomTransitionsWith18iOSApp.swift
+//  ZoomTransitionsWith18iOS
+//
+//  Created by test on 8/25/24.
+//
+
+import SwiftUI
+
+@main
+struct ZoomTransitionsWith18iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
